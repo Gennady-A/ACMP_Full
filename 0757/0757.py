58c71ca0b98adf075c7a0bbe6366050c218dcb0d
Gennady <gennadyaleynikov@gmail.com>
2022-09-26 16:19:54 +0300

    task 0757 completed

---
+++ 0757/0757.py
@@ -0,0 +1,5 @@
+with open('INPUT.txt', 'r') as inp:
+    nums = [int(i) for i in inp.read().split()]
+
+with open('OUTPUT.txt', 'w') as out:
+    out.write(str(min([nums[0]//2, nums[1]//6, nums[2]])))
